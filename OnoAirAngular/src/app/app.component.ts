519ea9d98fb3963f5bdc1dc71e96836710e30c56
Yakov <ymakoria@gmail.com>
2024-12-07 15:37:34 +0200

    שימוש ב angular material

--- OnoAirAngular/src/app/app.component.ts
+++ OnoAirAngular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- OnoAirAngular/src/app/header/header.component.ts
+++ OnoAirAngular/src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule,MatIconModule,MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  onMenuClick(): void {
+   alert("Menu Clicked");
+  }
 }
